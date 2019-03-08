478d5bed5bdc00c13aa4858006e975accdddd19b
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2019-03-08 18:04:29 +0000

    Client/server work, server multithreading incomplete

--- Project1/web-client.cpp
+++ Project1/web-client.cpp
@@ -21,65 +21,76 @@ int main(int argc, char *argv[])
     vector<string> parsedURL; // parsedURL[0] = hostname, parsedURL[1] = port, parsedURL[2] = resource name
     HttpRequest request;
 
+    if (argc <= 1){
+        cout << "Not enough arguments in function call" << endl;
+        return 0;
+    }
+
     for (int i = 1; i < argc; i++) {
         parsedURL = parseURL(argv[i]);
         request.setMethod("GET");
         request.setResource(parsedURL[2]);
 
+        string hostname = parsedURL[0];
+        string portNo = parsedURL[1];
+        string fileDir = parsedURL[2];
+        
+        struct addrinfo hints, *serverInfo;
+        int status, sockFD;
+        // struct sockaddr_storage their_addr;
+        // socklen_t addr_size;
+
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_flags = AI_PASSIVE; // use my IP
+
+
+
+        // DNS lookup and fills out the serverInfo struct
+        if ((status = getaddrinfo(hostname.c_str(), portNo.c_str(), &hints, &serverInfo)) != 0)
+        {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+            return 1;
+        }
+        
+        //returns a socket file descriptor to be used in later system calls
+        if ((sockFD = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol)) < 0){
+            perror("socket"); 
+            exit(EXIT_FAILURE); 
+        }
+
+        if (connect(sockFD, serverInfo->ai_addr, serverInfo->ai_addrlen) < 0){
+            perror("connect"); 
+            exit(EXIT_FAILURE); 
+        }
+
+        // send data to connection
+        if (send(sockFD, request.encode().c_str(), request.encode().length(), 0) < 0){
+            perror("send"); 
+            exit(EXIT_FAILURE); 
+        } 
+
+        //read the message into a buffer
+        // recv will block until there is some data to read
+        char buffer[BUFFER_SIZE] = {};
+        if (recv(sockFD, buffer, BUFFER_SIZE, 0) < 0){
+            perror("recv"); 
+            exit(EXIT_FAILURE); 
+        }
+        string temp(buffer);
+        int space = temp.find(" ")+1;
+        temp.erase(0, space);
+        string statusCode = temp.substr(0, temp.find(" "));
+        if (statusCode == "200")
+            writeToFile(buffer, fileDir);
+        else if (statusCode == "404")
+            cout << endl << "404 Not Found" << endl;
+        
+
+        close(sockFD);
+
     }
-
-    string hostname = parsedURL[0];
-    string portNo = parsedURL[1];
-    string fileDir = parsedURL[2];
-
-
-    struct addrinfo hints, *serverInfo;
-    int status, sockFD, new_sockFD;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-
-
-    // DNS lookup and fills out the serverInfo struct
-    if ((status = getaddrinfo(hostname.c_str(), portNo.c_str(), &hints, &serverInfo)) != 0)
-    {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return 1;
-    }
-    
-    //returns a socket file descriptor to be used in later system calls
-    if ((sockFD = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol)) < 0){
-        perror("socket"); 
-        exit(EXIT_FAILURE); 
-    }
-
-    if (connect(sockFD, serverInfo->ai_addr, serverInfo->ai_addrlen) < 0){
-        perror("connect"); 
-        exit(EXIT_FAILURE); 
-    }
-
-    // send data to connection
-    if (send(sockFD, request.encode().c_str(), request.encode().length(), 0) < 0){
-        perror("send"); 
-        exit(EXIT_FAILURE); 
-    } 
-
-    //read the message into a buffer
-    // recv will block until there is some data to read
-    char buffer[BUFFER_SIZE] = {};
-    if (recv(sockFD, buffer, BUFFER_SIZE, 0) < 0){
-        perror("recv"); 
-        exit(EXIT_FAILURE); 
-    }
-
-    writeToFile(buffer, fileDir);
-
-    close(sockFD);
 
     return 0;
 
@@ -95,10 +106,14 @@ vector<string> parseURL(string url)
     size_t found = url.find_first_of(":");
     string host = url.substr(0, found);
     output.push_back(host);
+    url = url.erase(0,found);
+
     size_t found1 = url.find_first_of("/");
-    string port = url.substr(found + 1, found1 - found - 1);
+    string port =url.substr(1,found1-1);
     output.push_back(port);
-    string resource = url.substr(found1);
+    url = url.erase(1,found1-1);
+
+    string resource =url.substr(2);;
     output.push_back(resource);
 
     return output;
@@ -107,14 +122,16 @@ vector<string> parseURL(string url)
 
 void writeToFile(char buffer[], string fileName){
 
-    // remove the "/" from the filename
-    fileName = fileName.substr(1, fileName.length());
+    // Parse out the body of the http response
+    string str(buffer);
+    string messageBody = str.substr(str.find("\n\n")+1, str.length());
+
     ofstream outfile ("client_" + fileName);
 
     if( !outfile )
         cout << endl <<  "Opening file failed" << endl;
     else{
-        outfile.write(buffer, 256);
+        outfile << messageBody;
         outfile.close();
     }
     return;
--- Project1/web-server.cpp
+++ Project1/web-server.cpp
@@ -10,12 +10,14 @@
 #include <sstream>
 #include <iterator>
 #include <vector>
+#include <pthread.h>
 
-#define BACKLOG 10     // how many pending connections queue will hold
-#define BUFFER_SIZE 5000 // max message buffer size
+#define BACKLOG 10       // how many pending connections queue will hold
+#define BUFFER_SIZE 50000 // max message buffer size
 
 vector<string> parseMessage(string buffer);
-int getFile(string filename, string& file, int& fileLength);
+int getFile(string filename, string &file, int &fileLength);
+void *socketThread(void *arg);
 
 // Program call:  web-server [hostname] [portNo] [fileDir]
 int main(int argc, char *argv[])
@@ -32,21 +34,24 @@ int main(int argc, char *argv[])
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; // use my IP
 
-    // Wrong number of arguments use default values 
-    if (argc < 4){
+    // Wrong number of arguments use default values
+    if (argc < 4)
+    {
         cout << "Using default values\n";
         hostname = (char *)"localhost";
         portNo = (char *)"4000";
         fileDir = (char *)".";
     }
-    else {
+    else
+    {
         hostname = argv[1];
-        portNo = argv[2]; 
+        portNo = argv[2];
         fileDir = argv[3];
     }
 
     // DNS lookup and fills out the serverInfo struct
-    if ( (status=getaddrinfo(hostname, portNo, &hints, &serverInfo) ) != 0 ){
+    if ((status = getaddrinfo(hostname, portNo, &hints, &serverInfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         return 1;
     }
@@ -57,64 +62,37 @@ int main(int argc, char *argv[])
     // Bind to port that was passed to getaddrinfo
     // All ports below 1024 are RESERVED
     bind(sockFD, serverInfo->ai_addr, serverInfo->ai_addrlen);
-    
+
     // listen for incoming connections
-    if (listen(sockFD, BACKLOG) < 0){
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
+    if (listen(sockFD, BACKLOG) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
     }
 
-    while(1){
+    pthread_t threadID[60];
+    int i = 0;
+    while (1)
+    {
         // accept an incoming connection:
         addr_size = sizeof(their_addr);
-        if ((new_sockFD = accept(sockFD, (struct sockaddr *)&their_addr, &addr_size)) < 0){
-            perror("accept"); 
-            exit(EXIT_FAILURE); 
+        if ((new_sockFD = accept(sockFD, (struct sockaddr *)&their_addr, &addr_size)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
         }
 
-        //read the message into a buffer
-        char buffer[BUFFER_SIZE] = {};
-        recv(new_sockFD, buffer, BUFFER_SIZE, 0);
-        
-        vector<string> temp = parseMessage(buffer);
-        string method, fileName;
-        // HTTP request format
-        // GET URL HTTP_V
-        method = temp[0];
-        fileName = temp[1];
-
-        cout << fileName << endl;
-
-        string f;
-        int fileLength;
-        if (method == "GET"){
-
-            int status = getFile(fileName, f, fileLength);
-            if (status == 200){
-                cout << f << endl;
-                // create response message
-                HttpResponse response;
-                response.setStatus(200);
-                response.setStatusMessage("OK");
-                response.setBody(f);
-                // send response message
-                send(new_sockFD, (response.encode()).c_str(), fileLength, 0); 
-
-            }
-            close(new_sockFD);
-        }
-
-        
-
-        
+        //for each new client request, create a thread and assign the handler to process it
+        if (pthread_create(&threadID[i], NULL, socketThread, &new_sockFD) != 0)
+            printf("Failed to create thread\n");
     }
-
 
     return 0;
 }
 
 // Splits a string input by spaces and returns a vector of words
-vector<string> parseMessage(string buffer) {
+vector<string> parseMessage(string buffer)
+{
     stringstream ss(buffer);
     istream_iterator<string> begin(ss);
     istream_iterator<string> end;
@@ -125,25 +103,65 @@ vector<string> parseMessage(string buffer) {
 
 // Function takes as arguments the name of file, string to write output to and filelength
 // returns an int 200, 400, 404
-int getFile(string filename, string& file, int& fileLength){
+int getFile(string filename, string &file, int &fileLength)
+{
     ifstream f;
     f.open("./" + filename);
 
     // check the file exists
-    if (f){
+    if (f)
+    {
         stringstream sstr;
         sstr << f.rdbuf();
         file = sstr.str();
         fileLength = file.length();
-
         return 200;
     }
     else
-        {
+    {
         cout << "404" << endl;
         return 404; // NOT FOUND
-        }
+    }
+}
 
-    
+void *socketThread(void *arg)
+{
+    int newSocket = *((int *)arg);
+
+    //read the message into a buffer
+    char buffer[BUFFER_SIZE] = {};
+    recv(newSocket, buffer, BUFFER_SIZE, 0);
+
+    vector<string> temp = parseMessage(buffer);
+    string method, fileName;
+    // HTTP request format
+    // GET URL HTTP_V
+    method = temp[0];
+    fileName = temp[1];
+
+
+    string f;
+    int fileLength;
+    if (method == "GET")
+    {
+        int status = getFile(fileName, f, fileLength);
+        HttpResponse response;
+        if (status == 200){
+            // create response message
+            response.setStatus(200);
+            response.setStatusMessage("OK");
+            response.setBody(f);
+            // send response message
+        }
+        else if (status == 404){
+            // create response message
+            response.setStatus(404);
+            response.setStatusMessage("Not Found");
+            response.setBody(f);
+        }
+        send(newSocket, (response.encode()).c_str(), fileLength+20, 0);
+
+        close(newSocket);
+    }
 
 }
